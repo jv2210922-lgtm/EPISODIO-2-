f0aa74fe5f6d58026a68c245c49407e2746b5030
jv2210922-lgtm <jv2210922@gmail.com>
2026-08-14 15:34:06 +0000

    primera clase oficial de python

---
+++ CLASE 1/ejercicio1.py
@@ -0,0 +1,5 @@
+print("hola")
+cocina = imput("mi nombre es marcos")
+print ("un placer cocnocerte me llamo mario") =cocina
+print("de donde eres?")
+cocina ("soy de argentina") = cocina
---
+++ CLASE 1/ejercicio2.py
@@ -0,0 +1,10 @@
+nombre = input("cual es el nombre de tu personaje?\n")
+raza = input ("de que raza es tu personaje?\n")
+clase = input (" de que clase es tu personaje\n")
+
+
+print ("===ficha de personaje===")
+print ("nombre:",nombre)
+print ("raza:",raza )
+print ("clase":,clase)
+
