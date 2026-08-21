4b408732f57a95a2e89d7f5e588814986602a28d
jv2210922-lgtm <jv2210922@gmail.com>
2026-08-21 15:28:33 +0000

    calculator is back

---
+++ ejercicio3.py
@@ -0,0 +1,11 @@
+print ("hola mundo") 
+
+mensaje = ("hola  Alumnos buen dia")
+
+print (mensaje)
+
+numero1=int(input("insertar el primer valor que quieres sumar") )
+numero2=int(input("insertar el segundo valor que quiers sumar "))
+suma= numero1+numero2
+print("la suma es :" , suma)
+numero 
